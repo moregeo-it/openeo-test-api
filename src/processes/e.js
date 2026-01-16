f25e10e605c1fcfc781107eaa7c375ea6d9807bc
niebl <caro.niebl@uni-muenster.de>
2026-01-16 18:43:56 +0100

    remove unused processes

--- src/processes/e.js
+++
@@ -1,9 +0,0 @@
-import GeeProcess from '../processgraph/process.js';
-
-export default class e extends GeeProcess {
-
-  executeSync(node) {
-    const ee = node.ee;
-    return ee.Number(Math.E);
-  }
-}
