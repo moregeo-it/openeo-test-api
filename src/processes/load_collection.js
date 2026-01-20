0c2cf8c37b862aace074869d24b97fec7a376dd6
niebl <caro.niebl@uni-muenster.de>
2026-01-20 15:11:00 +0100

    implement the use of a testimage

--- src/processes/load_collection.js
+++ src/processes/load_collection.js
@@ -1,16 +1,42 @@
 import GeeProcess from '../processgraph/process.js';
 import DataCube from '../datacube/datacube.js';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import PNG from 'pngjs';
 
 export default class load_collection extends GeeProcess {
+	async loadTestImageFromFile() {
+		const __filename = fileURLToPath(import.meta.url)
+    	const __dirname = path.dirname(__filename)
+		// load testimage.png from disk
+		const filePath = path.join(__dirname, '../../storage/testimage.png')
+		const fileBuffer = fs.readFileSync(filePath);
+		
+		return new Promise((resolve, reject) => {
+			const png = new PNG.PNG();
+			png.parse(fileBuffer, (err, data) => {
+				if (err) {
+					reject(err);
+				} else {
+					// Convert PNG data to Array
+					const pixelArray = Array.from(data.data);
+					resolve(pixelArray);
+				}
+			});
+		});
+	}
 
-	executeSync(node) {
-		// Create a dummy DataCube result for testing
-		//TODO: later to be replaced with a small example image from disk
-		const dummyDataCube = new DataCube(null);
-		dummyDataCube.setOutputFormat('JPEG');
-		dummyDataCube.setData([1,2,3,4,5]);
+	async executeSync(node) {
+		const id = node.getArgument('id');
+		const collection = node.getContext().getCollection(id);
 
-		return dummyDataCube;
+		let data
+		data = await this.loadTestImageFromFile();
+		
+		const dc = new DataCube(data);
+
+		return dc;
 	}
 
 }
