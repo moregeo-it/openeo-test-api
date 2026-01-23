cd9415ef7259f3bd5234d510c8b784bf66251d72
niebl <caro.niebl@uni-muenster.de>
2026-01-23 17:17:50 +0100

    implement missing processes

--- src/processes/array_element.js
+++ src/processes/array_element.js
@@ -2,7 +2,16 @@ import GeeProcess from '../processgraph/process.js';
 
 export default class array_element extends GeeProcess {
 
-  executeSync() {
-    return true
+  executeSync(node) {
+    let data = node.getArgument("data", undefined);
+    let index = node.getArgument("index", undefined);
+    
+    // Return the element at the specified index
+    if (Array.isArray(data) && index !== undefined) {
+      return data[index];
+    }
+    
+    // If not an array or no index, return undefined
+    return undefined;
   }
 }
--- src/processes/reduce_dimension.js
+++ src/processes/reduce_dimension.js
@@ -2,8 +2,22 @@ import GeeProcess from '../processgraph/process.js';
 
 export default class reduce_dimension extends GeeProcess {
 
-	async execute() {
-		return true
+	executeSync(node) {
+		let data = node.getArgument("data", undefined);
+		let reducer = node.getArgument("reducer", undefined);
+		
+		// If no reducer function is provided, return the data as-is
+		if (!reducer) {
+			return data;
+		}
+		
+		// If data is an array, apply the reducer across all elements
+		if (Array.isArray(data)) {
+			return data.reduce((acc, val) => reducer(acc, val));
+		}
+		
+		// If single value, return it
+		return data;
 	}
 
 }
