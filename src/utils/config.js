1e65faca4345ed6638fd415093e64c0664d25d74
niebl <caro.niebl@uni-muenster.de>
2026-02-02 14:01:51 +0100

    environment variable parsing

--- src/utils/config.js
+++ src/utils/config.js
@@ -6,7 +6,6 @@ import Utils from "./utils.js";
 export default class Config {
 
 	constructor() {
-<<<<<<< HEAD
 		this.environmentVariables = {
 			"debug": Boolean,
 			"production": Boolean,
@@ -22,23 +21,6 @@ export default class Config {
 			"defaultLogLevel": String,
 			"stacAssedDownloadSize": Number
 		}	
-=======
-		this.environmentVariableNames = [
-			"debug",
-			"production",
-			"hostname",
-			"apiPath",
-			"apiVersion",
-			"legacyTokens",
-			"id",
-			"title",
-			"description",
-			"port",
-			"exposePort",
-			"defaultLogLevel",
-			"stacAssedDownloadSize"
-		]	
->>>>>>> 4270d27 (improve environmentVariable checking)
 
 		// Set default that can be overriden by the config.json
 		this.debug = false;
